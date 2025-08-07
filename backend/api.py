3efeebb67744f20c60e40d860a9fc7b37ccd10bf
Vansh Chugh <1shhmusic2002@gmail.com>
2025-08-07 00:48:32 -0700

    Added CORS!

--- backend/api.py
+++ backend/api.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # Enable CORS for all origins (for local dev)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://whatcha-lookin-for.vercel.app/",
+    allow_origins=["https://whatcha-lookin-for.vercel.app",
                     "http://localhost:3000", "*"  # i'm not sure this is the link!
                     ],  # In production, set this to your frontend's URL, MAJOR TODO
     allow_credentials=True,
